a5c50bebef903c0aa335d8e0cee73f47e9198c4a
PongpichS <biggerbosssuper@gmail.com>
2020-09-10 21:15:19 +0700

    ooooooo

--- NumericalMethods/Practice5/problem7.m
+++ NumericalMethods/Practice5/problem7.m
@@ -1,3 +1,12 @@
 syms x
-r = @(x) sin(x).*cos(2.*x);
+r = @(x) exp(-x) - x;
 r1 = eval(['@(x)' vectorize(char(diff(r(x))))]);
+format long;
+x = 10;
+n = 6;
+i = 0;
+while i <= n
+    x = x - (r(x)/r1(x));
+    disp(x);
+    i = i+1;
+end
