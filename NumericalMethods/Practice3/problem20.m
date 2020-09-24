0b4e1aef10d3c8c7b1d9dfb720a559b81fd4a764
PongpichS <biggerbosssuper@gmail.com>
2020-09-24 13:01:19 +0700

    Yes

--- NumericalMethods/Practice3/problem20.m
+++ NumericalMethods/Practice3/problem20.m
@@ -49,7 +49,41 @@ subplot(4, 1, 4);
 f5(f2, -2*pi, 2*pi);
 
 %{
-23.Create an anonymous function, f6, that does the same 
-thing as Q22 but works with the anonymous function, f4, 
-which requires 2 inputs.
+23.Create a function file (​not anonymous​) that takes 3 inputs 
+and produces a surface plot. Thefirst input should be a function 
+that accepts 2 arguments (x and y), the second and third inputs
+should be vectors containing the desired values of x and y over 
+which to plot. Test yourfunction using the function f4 from 
+question 21 with x-values from 0 to 1 and y-values from 0to 4.
+%}
+
+%{
+24.Write a function file that takes between 3 and 6 input arguments 
+(all scalar values) and returnsa vector containing all the inputs 
+sorted from lowest to highest. Warning messages should bedisplayed 
+in the command window if the user enters the wrong nu                                                                                                                 mber of inputs, 
+or avector/string instead of a scalar.
+%}
+
+%{
+26.Write a function file that accepts any number of vector inputs 
+of the same length and plotseach of them in different colours against 
+the first vector on the same axes. Your code shouldcheck each of the 
+vectors is the same length or display a warning to the user.
+%}  
+
+%{
+27.Write a function file that accepts 3 input arguments, ​a​, ​b​ and ​c​. 
+If they are all different return 3output arguments equal to the input .
+values. If 2 are the same but 1 is different then return onlythe 2 unique 
+values. If all 3 are the same then return the unique value as a single 
+outputargument. Make sure to check the user asks for the right number of 
+outputs.
+%}
+
+%{
+28.Writea function file that takes one input and creates a random square 
+matrix of integers ofthat size. The outputs of the function should be the 
+mean of each column of the matrix. Makesure to check the required number 
+of outputs is requested by the user.
 %}
