db9652cb82c0a9ac6cda0262e44e00bef4b348ee
PongpichS <biggerbosssuper@gmail.com>
2020-09-27 14:23:53 +0700

    ff

--- NumericalMethods/Practice6/problem15.m
+++ NumericalMethods/Practice6/problem15.m
@@ -7,7 +7,7 @@ x(2) + 3*x(3) = -1
 2*x(1) - 10*x(2) + 2x(3) = -20
 %}
 A = [1, -3, 0; 0, 1, 3; 2, -10, 2];
-[L,U,P] = lu(A);
+[L,U] = lu(A);
 
 %{
 16.Solve the above system using LU factorisation but 
@@ -21,3 +21,5 @@ LU x = B
 Ux = L\B
 x = U\(L\B)
 %}
+x = inv(U) * inv(L) * B;
+x = U\(L\B)
