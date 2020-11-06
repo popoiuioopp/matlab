ce1c1e5090e81a663c7f665f32e259591779e071
PongpichS <biggerbosssuper@gmail.com>
2020-11-06 10:21:07 +0700

    3.7Done

--- NewWork/Work3_7.m
+++ NewWork/Work3_7.m
@@ -8,6 +8,21 @@
 x = 0.577;
 true_val = (6*x)/(1-3*x^2)^2;
 
-%{
-https://www.slader.com/discussion/question/compose-your-own-program-and-use-it-to-determine-your-computers-machine-epsilon/#
-%}
+% 3-Digits
+x6 = fix((6*x)/0.01) * 0.01;
+x2 = fix((x^2)/0.001) * 0.001;
+x23 = fix(3*(x2)/0.001) * 0.001;
+
+result_3digits = x6/(1-x23)^2;
+
+% 4-Digits
+x6 = 6*x;
+x2 = fix((x^2)/0.0001) * 0.0001;
+x23 = fix(3*(x2)/0.0001) * 0.0001;
+
+result_4digits = x6/(1-x23)^2;
+fprintf("\n3-Digits result = %f\n4-Digits result = %f\nTrue value = %f\n",...
+    result_3digits, result_4digits, true_val);
+fprintf("\nPercent Relative Error :\n3-Digits = %f%%\n4-Digits = %f%%\n", ...
+    abs(true_val-result_3digits)/true_val*100, abs(true_val-result_4digits)/true_val*100);
+fprintf("Chopping will create an error that effect the result a lot. The less chopping, the more error will be.\n");
---
+++ NewWork/Work3_8.m
@@ -0,0 +1,11 @@
+%{
+    3.8 (a) Evaluate the polynomial
+       y = x.^3 - 5.*x.^2 + 6.*x + 0.55;
+    at x = 1.37. Use 3-Digits arithmetic 
+    with chopping. Evaluate the percent 
+    relative error.
+    (b) Repeat (a) but express y as 
+       y = ((x-5).*x +6).*x+0.55;
+    evaluate the error and compare with 
+    part (a).
+%}
